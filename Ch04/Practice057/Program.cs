5f40dfbac78a3a4d3288a5ed2e0506bd1c1b14fa
Shun-Fa Yang <jackey890819@gmail.com>
2024-09-15 18:20:11 +0800

    feat: Practice057

---
+++ Ch04/Practice057/Program.cs
@@ -0,0 +1,19 @@
+﻿// 實例 057 靜態建置函數
+
+Console.WriteLine(Test.Sample);
+Console.WriteLine(Test.Sample);
+Console.WriteLine(Test.Sample);
+
+public class Test
+{
+    public static string Sample { get; }
+
+    /// <summary>
+    /// 靜態建置函數。用於初始化靜態成員。在任何靜態成員首次呼叫前會被呼叫，且往後不再被呼叫。
+    /// </summary>
+    static Test()
+    {
+        Sample = "示範屬性";
+        Console.WriteLine("靜態建置函數被呼叫。");
+    }
+}
