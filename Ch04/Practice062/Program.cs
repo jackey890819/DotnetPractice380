5f1470e79bf1e41ea0f30a0e002d0f276aeadc05
Shun-Fa Yang <jackey890819@gmail.com>
2024-09-15 21:08:00 +0800

    feat: Practice062

---
+++ Ch04/Practice062/Program.cs
@@ -0,0 +1,10 @@
+﻿// 實例 062 輸出參數
+
+double d1 = 1.25, d2 = 4.5, d3;
+Work(d1, d2, out d3);
+Console.WriteLine(d3);
+
+static void Work(double x, double y, out double result)
+{
+    result = x * y;
+}
