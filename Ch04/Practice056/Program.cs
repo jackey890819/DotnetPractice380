85aa04a944354472148b6b79b7b9f3cc0733be35
Shun-Fa Yang <jackey890819@gmail.com>
2024-09-15 17:47:20 +0800

    feat: Practice056

---
+++ Ch04/Practice056/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * 實例 056 建置函數的互相呼叫
+ */
+
+A a1 = new(100);
+A a2 = new();
+
+Console.WriteLine(a1.Num);
+Console.WriteLine(a2.Num);
+
+
+public class A
+{
+    public int Num;
+
+    public A(int num)
+    {
+        this.Num = num;
+    }
+    public A() : this(1000) { }
+}
