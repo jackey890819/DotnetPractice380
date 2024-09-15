f323cf66b772c74388bdecf99114264a0cb4b45e
Shun-Fa Yang <jackey890819@gmail.com>
2024-09-15 17:17:57 +0800

    feat: Practice 054

---
+++ Ch04/Practice054/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * 實例 054 宣告公共類別
+ */
+Ant v1 = default(Ant);          // 書中的宣告方式
+var v2 = default(Dragonfly);    // 使用var的宣告方式
+Spider? v3 = default;           // 個人比較愛用的方式(另外宣告為nullable)
+
+
+Console.WriteLine(v1 is null);
+Console.WriteLine(v2 is null);
+Console.WriteLine(v3 is null);
+
+
+public class Ant { }
+public class Dragonfly { }
+public class Spider { }
