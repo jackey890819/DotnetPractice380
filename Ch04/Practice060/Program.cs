b5a63b3e7c8a0b51c1183e19fc019735e218c7e2
Shun-Fa Yang <jackey890819@gmail.com>
2024-09-15 20:52:10 +0800

    feat: Practice060

---
+++ Ch04/Practice060/Program.cs
@@ -0,0 +1,22 @@
+﻿// 實例 060 多載方法
+
+Test t = new();
+Console.WriteLine(t.Compute(5));
+Console.WriteLine(t.Compute(5, 2));
+Console.WriteLine(t.Compute(5, 2, 3));
+
+public class Test
+{
+    public int Compute(int a)
+    {
+        return a * 1;
+    }
+    public int Compute(int a, int b)
+    {
+        return a * b;
+    }
+    public int Compute(int a, int b, int c)
+    {
+        return a * b * c;
+    }
+}
