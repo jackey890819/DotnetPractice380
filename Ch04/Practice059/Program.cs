8dc50969c64b716a80be3306d24aa959423e851b
Shun-Fa Yang <jackey890819@gmail.com>
2024-09-15 18:41:50 +0800

    feat: Practice059

---
+++ Ch04/Practice059/Program.cs
@@ -0,0 +1,19 @@
+﻿// 實例 059 初始化唯讀欄位
+
+SomeClass s1 = new("111");
+Console.WriteLine(s1.GenericKey);
+
+public class SomeClass
+{
+    public readonly string GenericKey;
+    public SomeClass(string genericKey)
+    {
+        GenericKey = genericKey;
+    }
+}
+
+// 新版方法：使用主要建構式
+//public class SomeClass(string genericKey)
+//{
+//    public readonly string GenericKey = genericKey;
+//}
